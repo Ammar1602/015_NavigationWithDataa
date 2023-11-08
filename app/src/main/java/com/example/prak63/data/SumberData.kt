c7af07a723a7d4d51a82ba0261b1216fd05b930f
Ammar Fatwa <ammar.fatwa.ft21@mail.umy.ac.id>
2023-11-08 15:39:50 +0700

    Initial commit

---
+++ app/src/main/java/com/example/prak63/data/SumberData.kt
@@ -0,0 +1,13 @@
+package com.example.prak63.data
+
+import com.example.prak63.R
+
+object SumberData {
+    val flavors = listOf(
+        R.string.natural,
+        R.string.vanilla,
+        R.string.lemon,
+        R.string.milk,
+        R.string.lyche,
+    )
+}
